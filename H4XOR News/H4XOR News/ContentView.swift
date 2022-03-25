fc38133f78b8770148c30bde76a48ae31d4d874a
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2022-03-25 06:34:50 +0530

    Update ContentView.swift

--- H4XOR News/H4XOR News/ContentView.swift
+++ H4XOR News/H4XOR News/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView{ // Navigation View (back to the previous screen)
+        
+        List{ // List view (Like Table view)
+            Text("Hello, world!")
+            Text("Hello, world!")
+        }
+            
+        .navigationTitle("H4XOR NEWS") // navigation bar title
+        }
+        
     }
 }
 
@@ -18,4 +27,13 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+// to store(get) Hacker News posts
+struct Post: Identifiable { // 'Identifiable' allow list to be able to recognize the oder of Post object based on 'ID' Veriable
+ 
+    let id: String // unique identify the post
+    let title: String // post title
+    
 }
