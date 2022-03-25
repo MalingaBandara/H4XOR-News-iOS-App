bcf01fa70c7b12a7967d969311fcfa00d5e42567
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2022-03-25 06:05:39 +0530

    project init

---
+++ H4XOR News/H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by Malinga Bandara on 2022-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ H4XOR News/H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Malinga Bandara on 2022-03-25.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
