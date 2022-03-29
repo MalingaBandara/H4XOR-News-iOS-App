f1ce72579ea6f466c82abb2b4e13e8bb43d8dcb1
Malinga Bandara <65806631+MalingaBandara@users.noreply.github.com>
2022-03-29 06:55:33 +0530

    Done

--- H4XOR News/H4XOR News/Views/DetailView.swift
+++ H4XOR News/H4XOR News/Views/DetailView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import WebKit
 
 
 struct DetailView: View {
@@ -22,34 +21,4 @@ struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(url: "https://www.google.com")
     }
-}
-
-
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String? // url
-    
-    func makeUIView(context: Context) -> WebView.UIViewType { // this is for create WebView Using WebKit. That componet is "WKWebView"
-        
-        return WKWebView() // WebKit component
-        
-    }
-     
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) { // this is for What item shoud put that WKWebView
-        
-        if let safeString = urlString{  // check the url null or not
-            
-            if let url = URL(string: safeString) { // set safeString to URL shoud load
-                
-                let request = URLRequest(url: url) // Create web request using URL
-                
-                uiView.load(request) // load the URL
-                
-            }
-            
-        }
-        
-    }
-    
 }
--- H4XOR News/H4XOR News/Views/WebView.swift
+++ H4XOR News/H4XOR News/Views/WebView.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import WebKit  // for web
+import SwiftUI
+
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String? // url
+    
+    func makeUIView(context: Context) -> WKWebView { // this is for create WebView Using WebKit. That componet is "WKWebView"
+        
+        return WKWebView() // WebKit component
+        
+    }
+     
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) { // this is for What item shoud put that WKWebView
+        
+        if let safeString = urlString{  // check the url null or not
+            
+            if let url = URL(string: safeString) { // set safeString to URL shoud load
+                
+                let request = URLRequest(url: url) // Create web request using URL
+                
+                uiView.load(request) // load the URL
+                
+            }
+            
+        }
+        
+    }
+    
+}
